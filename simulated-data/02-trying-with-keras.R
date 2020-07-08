bef17bc8d51e8a81dfb279aaee44dee72736d5dd
Lukas Burk <lukas@quantenbrot.de>
2020-07-09 01:00:00 +0200

    Re-try simulation data with keras due to fixed tensor shape

--- simulated-data/02-trying-with-keras.R
+++ simulated-data/02-trying-with-keras.R
@@ -4,8 +4,8 @@ library(acceleep)
 
 res <- 20             # Starting wit 20Hz instead of 100Hz or simplicity
 interval_length <- 30 # 30 seconds seems reasonable due to MET limitation
-n_children <- 50      #  50 "children"
-n_chunks <- 120       # Arbitrarily chosen, setting 120 results in 1 hour of simulated data
+n_children <- 20      # 20 "children"
+n_chunks <- 60        # Arbitrarily chosen, setting 120 results in 1 hour of simulated data
 n_chunks_total <- n_children * n_chunks
 
 # Generate 10 consecutive chunks at 20Hz, 30s per chunk
@@ -15,18 +15,16 @@ accel_sim_tbl <- generate_ts_dataset(
   n_chunks = n_chunks_total
 )
 
-accel_train <- convert_tbl_array(accel_sim_tbl[-1], interval_length, res)
+accel_train <- keras_reshape_accel(accel_sim_tbl[-1], interval_length, res)
 dim(accel_train) # 50 * n_chunks
 
 # Define labels (METs)
-train_labels <- rnorm(n_chunks_total, 0, 1) # Pretend we standardized them
-
+train_labels <- rnorm(n_chunks_total, 20, 10)
 
 # Define regression model
-
-model <- keras_model_sequential() %>%
-  layer_dense(units = 128, activation = "relu") %>%
-  layer_dense(units = 1)
+# model <- keras_model_sequential() %>%
+#   layer_dense(units = 128, activation = "relu") %>%
+#   layer_dense(units = 1)
 
 # Define LSTM model
 model <- keras_model_sequential() %>%
