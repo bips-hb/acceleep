46fc4b9fe4f9d96d7c353184b0044ff19275a31e
Lukas Burk <lukas@quantenbrot.de>
2020-07-08 23:58:26 +0200

    Attempting to use renv with python
    It's not going great, as the goal is platform/user-path independance.

--- modelling/00_setup.R
+++ modelling/00_setup.R
@@ -1,5 +1,26 @@
 # Installing keras et al
-# This file is only here in case there's important tweaks to the keras setup I need to keep track of down the road.
+# This file is only here in case there's important tweaks to the keras setup I
+# need to keep track of down the road.
+# Special consideration: using renv with this via renv::use_python()
+# Needs python < 3.8 for tensorflow afair
+
+# Installs miniconda for the system (_not_ renv specific)
+reticulate::install_miniconda(path = reticulate::miniconda_path())
+
+# Uses a specific conda end and records it in lockfile
+renv::use_python(
+  type = "conda", name = "acceleep"
+)
+
+# Some debug checkups
+# reticulate::use_condaenv()
+reticulate::conda_list()
+reticulate::conda_binary()
+reticulate::conda_python()
+
+# To install r and python packages from lockfile, use renv::restore() as usual
+
+# keras ----
 install.packages("keras")
 
 library(keras)
