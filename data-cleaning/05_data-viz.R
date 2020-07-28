c879d503f54d4d5e17329682934d47fbc5c81f30
Lukas Burk <burk@leibniz-bips.de>
2020-07-28 14:42:26 +0200

    Plot observed outcomes

---
+++ data-cleaning/05_data-viz.R
@@ -0,0 +1,37 @@
+library(ggplot2)
+library(dplyr)
+library(acceleep)
+
+full_data <- get_combined_data(model = "geneactiv", placement = "hip_right", res = 1)
+
+full_data %>%
+  select(ID, interval, kJ) %>%
+  group_by(ID, interval) %>%
+  summarize(outcome = unique(kJ), .groups = "drop") %>%
+  group_by(ID) %>%
+  mutate(index = seq_along(ID)) %>%
+  ggplot(aes(x = index, y = outcome)) +
+  facet_wrap(~ID) +
+  geom_path() +
+  theme_minimal()
+
+## from keras_prep_lstm
+
+# Split into train / validation datasets
+c(training_data, validation_data) %<-% make_initial_splits(
+  full_data, random_seed = 11235813, val_split = 1/3
+)
+
+# Split into data and labels
+split_data <- split_data_labels(training_data, validation_data, outcome = "kJ")
+
+c(train_data, train_labels) %<-% split_data$training
+c(test_data, test_labels) %<-% split_data$validation
+
+# Reshaping to array form
+train_data <- keras_reshape_accel(
+  accel_tbl = train_data, interval_length = interval_length, res = res
+)
+test_data <- keras_reshape_accel(
+  accel_tbl = test_data, interval_length = interval_length, res = res
+)
