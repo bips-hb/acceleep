8b9f8a4ec0d035a3c1add48fd45a5195f82f948f
Lukas Burk <burk@leibniz-bips.de>
2020-09-07 16:57:00 +0200

    Add CV functions

---
+++ R/cv.R
@@ -0,0 +1,45 @@
+#' Read CV results and bind them together
+#'
+#' @param path Path to CV results, e.g. `here::here("output/cross-validation/CNN")`.
+#'
+#' @return A tibble
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Iterate over all model output folders, read in latest CV runs, bind together
+#' cv_results <- purrr::map_df(
+#'  here::here("output/cross-validation", c("LM", "DNN", "CNN", "RNN")),
+#'   read_cv_results
+#' )
+#' }
+read_cv_results <- function(path) {
+  # browser()
+  # get latest run
+  path <- rev(sort(fs::dir_ls(path)))[[1]]
+
+  purrr::map_df(fs::dir_ls(path, glob = "*.rds"), ~{
+    tibble::tibble(
+      file = .x,
+      data = list(readRDS(.x) %>% dplyr::filter(!is.na(left_out)))
+    )
+  }) %>%
+    mutate(
+      file = fs::path_file(file) %>% fs::path_ext_remove(),
+      mean_rmse = purrr::map_dbl(data, ~mean(.x$rmse)),
+      median_rmse = purrr::map_dbl(data, ~median(.x$rmse)),
+      sd_rmse = purrr::map_dbl(data, ~sd(.x$rmse))
+    ) %>%
+    tidyr::separate(
+      col = .data$file,
+      into = c("folds", "method", "model_kind", "model", "placement", "outcome", "res", "timestamp"),
+      sep = "-"
+    ) %>%
+    dplyr::mutate(
+      accel_id = glue::glue("{.data$model}_{.data$placement}"),
+      model = label_accel_models(.data$model),
+      placement = purrr::map_chr(.data$placement, label_placement),
+      accel = glue::glue("{model} ({placement})")
+    ) %>%
+    dplyr::rename(outcome_unit = .data$outcome)
+}
--- cross-validation/cv_analysis.R
+++ cross-validation/cv_analysis.R
@@ -1,75 +1,165 @@
 library(acceleep)
 library(ggplot2)
 library(dplyr)
+library(tidyr)
 library(kableExtra)
 
-rf_results_paper <- tibble::tribble(
-  ~outcome, ~model,      ~placement,    ~mean_rmse, ~sd_rmse,
-  "kJ",     "actigraph", "hip_left",    2.6,        0.97,
-  "Jrel",   "actigraph", "hip_left",    112.32,     28.40,
-  "MET",    "actigraph", "hip_left",    1.52,       0.38,
-  "kJ",     "actigraph", "hip_right",   2.74,       0.96,
-  "Jrel",   "actigraph", "hip_right",   115.56,     27.35,
-  "MET",    "actigraph", "hip_right",   1.56,       0.36,
-  "kJ",     "geneactiv", "hip_right",   2.73,       1,
-  "Jrel",   "geneactiv", "hip_right",   112.57,     28.83,
-  "MET",    "geneactiv", "hip_right",   1.53,       0.38,
-  "kJ",     "geneactiv", "wrist_left",  2.56,       0.83,
-  "Jrel",   "geneactiv", "wrist_left",  108.64,     26.33,
-  "MET",    "geneactiv", "wrist_left",  1.47,       0.36,
-  "kJ",     "geneactiv", "wrist_right", 2.56,       0.83,
-  "Jrel",   "geneactiv", "wrist_right", 109.34,     26.98,
-  "MET",    "geneactiv", "wrist_right", 1.48,       0.37
+# Iterate over all model output folders, read in latest CV runs, bind together
+cv_results <- purrr::map_df(
+  here::here("output/cross-validation", c("LM", "RF", "DNN", "CNN", "RNN")),
+  read_cv_results
 )
 
-cv_files <- fs::dir_ls(here::here("output/cross-validation/LM/20200901170538/"), glob = "*.rds")
-
-results <- purrr::map_df(cv_files, ~{
-  tibble::tibble(
-    file = .x,
-    data = list(readRDS(.x) %>% filter(!is.na(left_out)))
-  )
-}) %>%
-  mutate(
-    file = fs::path_file(file) %>% fs::path_ext_remove(),
-    mean_rmse = purrr::map_dbl(data, ~mean(.x$rmse)),
-    median_rmse = purrr::map_dbl(data, ~median(.x$rmse)),
-    sd_rmse = purrr::map_dbl(data, ~sd(.x$rmse))
-  ) %>%
-  tidyr::separate(
-    col = file,
-    into = c("folds", "method", "model_kind", "model", "placement", "outcome", "res", "timestamp"),
-    sep = "-"
-  ) %>%
-  mutate(
-    model = label_accel_models(model),
-    placement = purrr::map_chr(placement, label_placement),
-    accel = glue::glue("{model} ({placement})")
-  )
-
 # A plot ----
-results %>%
+cv_results %>%
   tidyr::unnest(data) %>%
-  ggplot(aes(x = accel, y = rmse)) +
-  facet_grid(rows = vars(outcome), scales = "free_y") +
-  geom_boxplot() +
-  theme_minimal()
+  ggplot(aes(x = reorder(model_kind, rmse), y = rmse)) +
+  facet_grid(cols = vars(accel), rows = vars(outcome_unit), scales = "free_y") +
+  geom_point(size = 1, alpha = .2, position = position_jitter(width = .15, seed = 23)) +
+  geom_boxplot(alpha = .5, outlier.alpha = 0) +
+  stat_summary(geom = "point", fun = mean, fill = "red", shape = 21, color = "black", size = 2) +
+  labs(
+    title = "Leave-One-Subject-Out Cross Validation",
+    subtitle = "Across all subjects in the respective training set (2/3 of complete data)",
+    x = "Model Type", y = "Per-Subject RMSE (+ Mean)"
+  ) +
+  hrbrthemes::theme_ipsum_pub(grid = "Y")
 
-results %>%
+ggsave(filename = here::here("LOSO-CV-boxplot-20200907.pdf"), device = cairo_pdf, scale = 2, width = 10, height = 5)
+
+# predict() vs evaluate()
+cv_results %>%
   tidyr::unnest(data) %>%
-  mutate(prediction_diff = (rmse - prediction_rmse)^2) %>%
+  filter(!is.na(eval_rmse)) %>%
+  mutate(prediction_diff = (rmse - eval_rmse)^2) %>%
   arrange(desc(prediction_diff)) %>%
   summarize(rmse_rmse = sqrt(mean(prediction_diff)))
 
+cv_results %>%
+  tidyr::unnest(data) %>%
+  filter(!is.na(eval_rmse)) %>%
+  mutate(prediction_diff = rmse - eval_rmse) %>%
+  ggplot(aes(x = prediction_diff)) +
+  facet_grid(cols = vars(outcome_unit), rows = vars(model_kind), scales = "free") +
+  geom_histogram(color = "darkgray", alpha = 2/3) +
+  labs(
+    title = "Difference in RMSE Estimation",
+    subtitle = "Difference in RMSE as calculated by keras::evaluate() vs. manual calculation with predict()",
+    x = "Absolute difference in RMSE: predict() - evaluate()",
+    y = "Frequency"
+  ) +
+  hrbrthemes::theme_ipsum_ps(grid = "Y")
+
 # A table ----
-results %>%
-  transmute(
-    accel = accel,
-    outcome = outcome,
+# cv_results %>%
+#   transmute(
+#     model_kind = model_kind,
+#     accel = accel,
+#     outcome_unit = outcome_unit,
+#     measure = glue::glue("{round(mean_rmse, 2)} ({round(sd_rmse, 2)})") %>% as.character()
+#   ) %>%
+#   tidyr::pivot_wider(names_from = accel, values_from = measure) %>%
+#   #slice(c(3, 1, 2)) %>%
+#   kable(caption = glue::glue("LOSO-CV results, Mean RMSE (SD)")) %>%
+#   kable_styling() %>%
+#   collapse_rows(columns = 1)
+
+cv_results %>%
+  mutate(
     measure = glue::glue("{round(mean_rmse, 2)} ({round(sd_rmse, 2)})") %>% as.character()
   ) %>%
-  tidyr::pivot_wider(names_from = accel, values_from = measure) %>%
-  slice(c(3, 1, 2)) %>%
-  kable(caption = glue::glue("{unique(results$model_kind)} LOSO-CV results, Mean RMSE (SD)")) %>%
-  kable_styling()
+  group_by(accel, outcome_unit) %>%
+  mutate(
+    # min_rmse = min(mean_rmse),
+    is_min_mean_rmse = mean_rmse == min(mean_rmse)
+  ) %>%
+  ungroup() %>%
+  select(
+    model_kind,
+    accel,
+    outcome_unit,
+    measure,
+    is_min_mean_rmse
+  ) %>%
+  mutate(measure = cell_spec(measure, bold = is_min_mean_rmse)) %>%
+  select(-is_min_mean_rmse) %>%
+  pivot_wider(id_cols = c(model_kind, accel, outcome_unit), names_from = model_kind, values_from = measure) %>%
+  rename(Accelerometer = accel, Outcome = outcome_unit) %>%
+  kable(
+    caption = glue::glue("LOSO-CV results: Mean RMSE (SD)"), escape = FALSE
+  ) %>%
+  kable_styling() %>%
+  #footnote("Bold: Minimal RMSE per Accelerometer and Outcome") %>%
+  collapse_rows(columns = 1)
+
+# Per model per subject predictions -----
+
+vec_model_kind <- unique(cv_results$model_kind)
+vec_accel <- unique(cv_results$accel)
+vec_outcome_unit <- unique(cv_results$outcome_unit)
+
+
+for (current_model_kind in vec_model_kind) {
+  for (current_accel in vec_accel) {
+    for (current_outcome_unit in vec_outcome_unit) {
+      current_result <- cv_results %>%
+        filter(
+          model_kind == .env$current_model_kind,
+          accel == .env$current_accel,
+          outcome_unit == .env$current_outcome_unit
+        )
+
+      # out_dir <- here::here("output", "cross-validation", current_model_kind, current_result$timestamp, "plots")
+      out_dir <- here::here("output", "cross-validation", "plots")
+      filename <- glue::glue("k1-cv-predictions-{current_model_kind}-{current_result$accel_id}-{current_result$outcome_unit}-{current_result$res}-{current_result$timestamp}.png")
+
+      # print(out_dir)
+      # print(filename)
+
+      cliapp::cli_alert_info("Plotting {current_model_kind} / {current_accel} / {current_outcome_unit} to {filename}")
+
+      current_result %>%
+        unnest(data) %>%
+        unnest(predicted_obs) %>%
+        pivot_longer(cols = c(outcome, predicted)) %>%
+        ggplot(aes(x = interval, y = value, color = name)) +
+        facet_wrap(~left_out, scales = "free_x") +
+        geom_path(key_glyph = "rect") +
+        scale_color_brewer(palette = "Dark2", labels = c("outcome" = "Observed", "predicted" = "Predicted")) +
+        labs(
+          title = glue::glue("{current_model_kind} Per-Subject Predictions for {current_accel}"),
+          subtitle = "Predictions based on model for which respective subject is not in training data",
+          x = "Interval Index (30s)", y = current_outcome_unit, color = ""
+        ) +
+        hrbrthemes::theme_ipsum_pub(
+          base_size = 14,
+          strip_text_size = 11,
+          axis_title_size = 13,
+          axis_text_size = 10,
+          grid = "Y"
+        ) +
+        theme(legend.position = "top")
+
+      # ggsave(
+      #   filename = fs::path(out_dir, fs::path_ext_set(filename, "pdf")),
+      #   # device = ragg::agg_png, units = "cm", width = 40, height = 25,
+      #   device = cairo_pdf, width = 20, height = 12.5,
+      #   dpi = 320
+      # )
+      ggsave(
+        filename = fs::path(out_dir, fs::path_ext_set(filename, "png")),
+        device = ragg::agg_png, units = "cm", width = 40, height = 25,
+        # device = cairo_pdf, width = 20, height = 12.5,
+        dpi = 320
+      )
+
+    }
+  }
+}
+
+
+ggsave(filename = here::here("LOSO-CV-boxplot-20200907.pdf"), device = cairo_pdf, scale = 2, width = 10, height = 5)
+
+
+filename <- glue::glue("k1-cv-{model_kind}-{metaparams$model}-{metaparams$placement}-{metaparams$outcome}-{metaparams$res}-{run_start}.rds")
 
