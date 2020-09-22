2c5ad7aa2fe89193a1589c272384099fe4060f94
Lukas Burk <burk@leibniz-bips.de>
2020-09-22 12:41:26 +0200

    Start preparing final (full) LOSO-CV

---
+++ cross-validation-full/full-k1-cv-lm.R
@@ -0,0 +1,149 @@
+#! /usr/bin/env Rscript
+library(dplyr)
+library(acceleep)
+library(cliapp)
+
+
+tick <- Sys.time()
+# Declaring metadata ----
+model_kind <- "LM"
+run_start <- format(tick, '%Y%m%d%H%M%S')
+cliapp::cli_alert_info("Starting {model_kind} LOSO-CV on {run_start}")
+
+metadata <- get_overview_table() %>%
+  distinct(model, placement) %>%
+  tidyr::expand_grid(outcome = c("kJ", "Jrel", "MET")) %>%
+  mutate(res = ifelse(model == "activpal", 20, 100))
+
+# Big loop over accelerometers, placements, outcomes
+for (row in seq_len(nrow(metadata))) {
+  # hold current metadata
+  metaparams <- metadata[row, ]
+  cliapp::cli_alert_info("Starting {model_kind} on {metaparams$model} ({metaparams$placement}) / {metaparams$outcome}")
+
+  # browser()
+
+  # Collecting original training data only to get it's subject IDs
+  # resolution is small here because it doesn't matter, actual training data is read later
+  c(c(train_data_full, train_labels_full), c(., .)) %<-% keras_prep_regression(
+    model = metaparams$model, placement = metaparams$placement,
+    outcome = metaparams$outcome, random_seed = 19283, val_split = 0,
+    interval_length = 30, res = metaparams$res, normalize = FALSE
+  )
+
+  # This is easier for later subject-splitting I assume.
+  train_data_full$outcome <- train_labels_full
+  train_data_full <- train_data_full %>%
+    select(ID, interval, outcome, everything())
+
+  IDs_full <- train_data_full %>%
+    pull(.data$ID) %>%
+    unique() %>%
+    sort()
+
+  # Initialize empty tibble to collect results
+  cv_result <- tibble::tibble()
+
+  prog <- cli_progress_bar(
+    format = ":current of :total [:bar] (:percent, :elapsedfull)",
+    total = length(IDs_full)
+  )
+
+  # Loop over subject IDs ----
+  # leave out one for each model run
+  for (i in IDs_full) {
+    prog$tick()
+
+    # Dataprep ----
+    # Split into train / validation datasets based on subject IDs
+    training_data <- train_data_full %>%
+      filter(.data$ID %in% IDs_full, .data$ID != i)
+
+    # Fail if for some reason left out ID is in training set
+    stopifnot(!(i %in% unique(training_data$ID)))
+
+    test_data <- train_data_full %>%
+      filter(.data$ID == i)
+
+    # Fail if left out ID is _not_ in validation data where it belongs
+    stopifnot(i %in% unique(test_data$ID))
+
+    # Normalize data
+    training_means <- training_data %>%
+      select(-c("ID", "interval", "outcome")) %>%
+      purrr::map_dbl(mean)
+
+    training_sds <- training_data %>%
+      select(-c("ID", "interval", "outcome")) %>%
+      purrr::map_dbl(sd)
+
+    training_data[-c(1:3)] <- scale(training_data[-c(1:3)], center = training_means, scale =  training_sds)
+    test_data[-c(1:3)] <- scale(test_data[-c(1:3)], center = training_means, scale =  training_sds)
+
+
+    # Check
+    unique(training_data$ID)
+    unique(test_data$ID)
+
+    dim(training_data)
+    dim(test_data)
+
+    # Modelling ----
+    model_tick <- Sys.time()
+
+    model <- lm(outcome ~ ., data = training_data[-c(1:2)])
+
+    # To check in with LOO model results
+    # browser()
+
+    # Evaluate, save results
+    # Make predictions
+    predicted_obs <- test_data %>%
+      select(ID, interval, outcome) %>%
+      mutate(
+        predicted = as.numeric(predict(model, test_data[-c(1:3)]))
+      )
+
+    # prediction rmse differs from result of evaluate() o_O
+    prediction_rmse <- predicted_obs %>%
+      summarize(rmse = sqrt(mean((predicted - outcome)^2))) %>%
+      pull(rmse)
+
+    model_tock <- Sys.time()
+    model_took <- hms::hms(seconds = round(as.numeric(difftime(model_tock, model_tick, units = "secs"))))
+
+    current_result <- tibble::tibble(
+      left_out = i,
+      rmse = prediction_rmse,
+      predicted_obs = list(predicted_obs),
+      model_took = model_took
+    )
+
+    cv_result <- bind_rows(cv_result, current_result)
+
+    # Save per-subject model maybe?
+    out_dir_models <- here::here("output", "cross-validation-full", model_kind, run_start, "models")
+    if (!fs::dir_exists(out_dir_models)) fs::dir_create(out_dir_models)
+    filename_model <- glue::glue("k1-cv-{model_kind}-{metaparams$model}-{metaparams$placement}-{metaparams$outcome}-{metaparams$res}-LOSO_{i}-{run_start}.rds")
+    saveRDS(model, file = fs::path(out_dir_models, filename_model))
+  }
+
+
+  # Save result tibble
+  filename <- glue::glue("k1-cv-{model_kind}-{metaparams$model}-{metaparams$placement}-{metaparams$outcome}-{metaparams$res}-{run_start}.rds")
+
+  out_dir <- here::here("output", "cross-validation-full", model_kind, run_start)
+  if (!fs::dir_exists(out_dir)) fs::dir_create(out_dir)
+
+  # Save CV RMSE results
+  saveRDS(object = cv_result, file = fs::path(out_dir, filename))
+
+
+  # Write model structure to plain text file
+  capture.output(summary(model), file =  fs::path(out_dir, fs::path_ext_set(filename, "txt")))
+}
+
+tock <- Sys.time()
+took <- hms::hms(seconds = round(as.numeric(difftime(tock, tick, units = "secs"))))
+pushoverr::pushover(glue::glue("{model_kind} cross validation is done! Took {took}"), title = "Modelling Hell", priority = 1)
+
