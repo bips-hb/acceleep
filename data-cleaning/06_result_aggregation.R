e34b0d36cea6f5ed8b4bd0ed81c9b4ae2beb7b42
Lukas Burk <lukas@quantenbrot.de>
2020-10-05 14:24:29 +0200

    Aggregate results in output dataset

---
+++ data-cleaning/06_result_aggregation.R
@@ -0,0 +1,52 @@
+# Aggregation of results from various validation methods.
+# Collect and save results for convenient access in text
+library(dplyr)
+library(acceleep)
+
+# Directory to save result data to
+usethis::use_directory("output/results")
+
+# Holdout results ----
+holdout_results <- purrr::map_df(
+  here::here("output/holdout-validation", c("LM", "RF", "DNN", "CNN", "RNN")),
+  ~read_holdout_results(.x)
+) %>%
+  mutate(
+    model_kind = ifelse(model_kind == "RNN", glue::glue("{model_kind} ({res}Hz)"), model_kind),
+    model_kind = ifelse(model_kind %in% c("RNN (100Hz)", "RNN (20Hz)"), "RNN (100Hz/20Hz)", model_kind)
+  )
+
+
+saveRDS(holdout_results, file = here::here("output", "results", "holdout.rds"))
+
+# Development CV ----
+
+current_best <- tibble::tribble(
+  ~model_kind, ~timestamp,
+  "CNN",  20200921164853,
+  "DNN",  20200923144304,
+  "RNN (1Hz)",  20200917020316,
+  "RNN (10Hz)", 20200919233628,
+  "RF",  20200910115426,
+  "LM",  20200903163311,
+)
+
+# Iterate over all model output folders, read in latest CV runs, bind together
+cv_results <- purrr::map_df(
+  here::here("output/cross-validation", c("LM", "RF", "DNN", "CNN", "RNN")),
+  ~read_cv_results(.x, latest_only = FALSE)
+) %>%
+  mutate(
+    current_best = (timestamp %in% current_best$timestamp),
+    model_kind = ifelse(model_kind == "RNN", glue::glue("{model_kind} ({res}Hz)"), model_kind)
+  )
+
+saveRDS(cv_results, file = here::here("output", "results", "cv_results.rds"))
+
+
+# Full CV (incomplete)
+
+full_cv_results <- purrr::map_df(
+  here::here("output/cross-validation-full", c("LM", "RF")),
+  ~read_cv_results(.x, latest_only = FALSE)
+)
